482fd6c83500e2fc56a869ca29b99e24066e4790
Muhammad Fajri Rasid <fajrimuhammad460@gmail.com>
2020-10-04 21:20:40 +0800

    update js and html

--- login_form/js/script.js
+++ login_form/js/script.js
@@ -1,44 +1,64 @@
 // style show and hide password
-// select class fas fa-eye
+// select class "fas toggle"
 const eye = document.querySelector(".fas.toggle");
 
-// select class password
+// select class "password"
 const password = document.getElementById("password");
 
+// saat element dengan class eye di klik
 eye.addEventListener("click", () => {
+	// ubah iterable classList menjadi array
 	const classesEye = Array.from(eye.classList);
 
+	// jika classList eye yang ada pada index ke 2 = "fa-eye-slash"
 	if (classesEye[2] === "fa-eye-slash") {
+		// ganti type input menjadi text agar password kelihatan
 		password.type = "text";
+		// hapus class/icon fa-eye-slash
 		eye.classList.remove("fa-eye-slash");
+		// tambahkan class/icon fa-eye
 		eye.classList.add("fa-eye");
-	} else {
+	}
+	// jika classList eye yang ada pada index ke 2 != "fa-eye-slash"
+	else {
+		// ganti type input menjadi password agar password tidak kelihatan
 		password.type = "password";
+		// hapus class/icon fa-eye
 		eye.classList.remove("fa-eye");
+		// tambahkan class/icon fa-eye-slash
 		eye.classList.add("fa-eye-slash");
 	}
 });
 
-// style pada input type text
-// select input type=text
+// style pada input
+// select element dengan class ".input-user"
 const input = document.querySelectorAll(".input-user");
 
 // select element form
 const parent = document.getElementsByTagName("form")[0];
 
+// saat semua bagian dari parent di klik
 parent.addEventListener("click", (event) => {
+	// select semua element dengan class "line"
 	const mainLine = document.querySelectorAll(".line");
 
+	// jika element yang di click memiliki class "username" pada index ke-1 dari classlistnya, maka:
 	if (event.target.classList[1] === "username") {
-		// select kelas "line first"
-		const line = document.querySelector(".first");
+		// select class "line first"
+		const line = document.querySelector(".line.first");
+		// beri animasi
 		line.style.animation = "input-animation 0.5s ease forwards";
-	} else if (event.target.classList[1] === "password") {
-		// select kelas "line second"
-		const line = document.querySelector(".second");
+	}
+	// namun, jika element yang di click memiliki class "password" pada index ke-1 dari classlistnya, maka:
+	else if (event.target.classList[1] === "password") {
+		// select class "line second"
+		const line = document.querySelector(".line.second");
+		//beri animasi
 		line.style.animation = "input-animation 0.5s ease forwards";
 	}
 
+	// tunggu 0.5s, kemudian value dari semua line diubah menjadi none, agar
+	// animasi bisa berjalan lagi ketika user kembali mengklik input username/password
 	setTimeout(() => {
 		mainLine.forEach((element) => {
 			element.style.animation = "none";
