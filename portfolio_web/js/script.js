262d77af09e19801287cc0d1c4d9cdf8dce02bac
Muhammad Fajri Rasid <fajrimuhammad460@gmail.com>
2020-10-09 08:44:38 +0800

    adding jquery

--- portfolio_web/js/script.js
+++ portfolio_web/js/script.js
@@ -1,3 +1,38 @@
-$(".scroll").on("click", function () {
-	console.log("ok");
+// smooth scrolling effect
+$(".scroll").on("click", function (e) {
+	// ambil isi dari attribute href dari class scroll sesuai yang di click
+	const href = $(this).attr("href");
+	// section target dari href
+	const target = $(href);
+	// animasi scroll
+	$("html, body").animate(
+		{
+			scrollTop: target.offset().top,
+		},
+		900,
+		"easeInOutExpo"
+	);
+	// hilangkan event default dari class scroll
+	e.preventDefault();
+	// console.log($("html, body").scrollTop());
+	// console.log(target.offset().top);
+});
+
+// scroll top button effect
+$(window).on("scroll", function () {
+	if ($(this).scrollTop() > 700) {
+		$(".section-on-top").fadeIn(500);
+	} else {
+		$(".section-on-top").fadeOut(500);
+	}
+});
+
+$(".section-on-top").on("click", function () {
+	$("html, body").animate(
+		{
+			scrollTop: 0,
+		},
+		900,
+		"easeInOutExpo"
+	);
 });
