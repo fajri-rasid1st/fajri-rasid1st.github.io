77db3c18b1bdee85af45f08858449da35e128f1e
Muhammad Fajri Rasid <fajrimuhammad460@gmail.com>
2020-10-18 09:44:03 +0800

    edit html css js

--- movie_api/js/script.js
+++ movie_api/js/script.js
@@ -1,99 +1,99 @@
 // function untuk mengambil data film pada API
-function getDataFilms(name) {
+function getDataFilms(name, option) {
 	// request API dengan menggunakan method ajax
 	$.ajax({
 		// url dari API untuk search
-		url: `https://www.omdbapi.com/?apikey=6dcfe9e9&s=${name}`,
+		url: `https://www.omdbapi.com/?apikey=6dcfe9e9&s=${name}&type=${option}`,
 		// saat sukses, lakukan ini
 		success: (result) => {
 			// ambil hasilnya. list Search berupa list dengan kumpulan objek.
 			const movies = result.Search;
 			// jika movies tidak ada atau kosong
 			if (movies === undefined) {
+				// kosongkan isi movie-container
+				$(".movie-container").html("");
 				// tampilkan alert
-				alert("Film Not Found!");
+				alert("Film Not Found! :)");
 			}
 			// jika movies ada
 			else {
 				// display dummy text dihilangkan
 				$(".dummy-text").css("display", "none");
+				// ubah display text search-result menjadi block pada main section
+				$(".search-result").css("display", "block");
+				// // ubah justify content movie container
+				// $(".movie-container").css("justify-content", "flex-start");
 				// deklarasikan cards kosong
 				let cards = "";
-				// ubah beberapa style pada main section
-				$(".search-result").css("display", "block");
 				// untuk setiap movie pada movies
 				movies.forEach((movie) => {
-					// tambahkan HTML Fragments untuk cards
+					// tambahkan HTML Fragments untuk setiap card
 					cards += cardHTMLFragments(movie);
 				});
 				// isi dari movie-container diisi dengan variable cards
 				$(".movie-container").html(cards);
-				// saat tombol show details di klick
-				$(".show-details").on("click", function () {
-					// request API dengan menggunakan method ajax
-					$.ajax({
-						// url untuk mengambil detail film
-						url: `https://www.omdbapi.com/?apikey=6dcfe9e9&i=${$(
-							this
-						).data("imdbid")}`,
-						// saat sukses, lakukan
-						success: (details) => {
-							// tampung isi details pada variable movieDtails
-							const movieDetails = modalHTMLFragments(details);
-							// modal diisi dengan fragment pada movieDetails
-							$(".modal-body").html(movieDetails);
-						},
-						// saat error, lakukan
-						error: (err) => {
-							console.log(err.responseText);
-						},
-					});
-				});
+				// panggil function getDetailFilm
+				getDetailFilm();
 			}
 		},
 		// saat error, lakukan
-		error: (err) => {
-			console.log(err.responseText);
+		error: () => {
+			alert("Film Not Found!");
 		},
 	});
 }
-// saat tombol button submit di klick
-$(".submit").on("click", (e) => {
-	// hilangkan aksi defaultnya
-	e.preventDefault();
-	// ambil value dari search input
-	const userSearch = $(".search-input").val();
-	// kalau inputan user tidak kosong
-	if (userSearch.trim().length != 0) {
-		// call the getDataFilms function
-		getDataFilms(userSearch);
-	}
-});
+
+// function untuk melihat detail film tertentu (memerlukan request API)
+function getDetailFilm() {
+	// saat tombol show details di klick
+	$(".show-details").on("click", function () {
+		// request API dengan menggunakan method ajax
+		$.ajax({
+			// url untuk mengambil detail film
+			url: `https://www.omdbapi.com/?apikey=6dcfe9e9&i=${$(this).data(
+				"imdbid"
+			)}&plot=full`,
+			// saat sukses, lakukan
+			success: (details) => {
+				// tampung isi details pada variable movieDetails
+				const movieDetails = modalHTMLFragments(details);
+				// modal diisi dengan fragment pada movieDetails
+				$(".modal-body").html(movieDetails);
+			},
+			// saat error, lakukan
+			error: () => {
+				alert("Film Not Found!");
+			},
+		});
+	});
+}
+
 // function untuk mengembalikan HTML Fragments dari card
 function cardHTMLFragments(movie) {
 	return `
-	<div class="col-md-4 mb-4 py-0 col-result">
+	<div class="col col-result px-0">
 		<div class="card">
-			<img src="${movie.Poster}" class="card-img-top" height=475/>
+			<img src="${movie.Poster}" class="card-img-top" height=400/>
 			<div class="card-body">
 				<h6 class="card-title">${movie.Title}</h6>
-				<h6 class="card-subtitle mb-2" style="opacity: 0.8; font-weight: 300">
+				<h6 class="card-subtitle mb-2" style="opacity: 0.8; font-weight: 400">
 					${movie.Year}
 				</h6>
 				<a href="#" class="btn btn-outline-dark show-details" data-toggle="modal"
-				data-target="#show-movie-detail" data-imdbid=${movie.imdbID}>
+					data-target="#show-movie-detail" data-imdbid=${movie.imdbID}>
 					Show Details
 				</a>
 			</div>
 		</div>
 	</div>`;
 }
+
 // function yang mengembalikan HTML Fragments yang berisi detail film yang di klik
 function modalHTMLFragments(details) {
 	return `
 	<div class="container-fuild">
-		<div class="row">
-			<div class="col-md-3 pr-0">
+		<div class="row justify-content-center align-content-center">
+			<div class="col-md-4 text-center">
 				<img src="${details.Poster}" alt="${details.Poster}" class="img-fluid" style="border-radius: 5px"/>
 			</div>
 			<div class="col-md">
@@ -124,3 +124,35 @@ function modalHTMLFragments(details) {
 		</div>
 	</div>`;
 }
+
+// saat tombol button submit di klick
+$(".submit").on("click", () => {
+	// ambil value dari search input
+	const userSearch = $(".search-input").val();
+	// kalau inputan user tidak kosong
+	if (userSearch.trim().length != 0) {
+		// call the getDataFilms function
+		getDataFilms(userSearch, "");
+	}
+});
+
+// saat salah satu tombol option di klick
+$(".option").on("click", (event) => {
+	// ambil value dari search input
+	const userSearch = $(".search-input").val();
+	// kalau inputan user tidak kosong
+	if (userSearch.trim().length != 0) {
+		// kalau option yang di klick valuenya adalah Movie
+		if (event.target.textContent.trim() === "Movie") {
+			getDataFilms(userSearch, "Movie");
+		}
+		// kalau option yang di klick valuenya adalah Series
+		else if (event.target.textContent.trim() === "Series") {
+			getDataFilms(userSearch, "Series");
+		}
+		// kalau option yang di klick valuenya adalah Episode
+		else if (event.target.textContent.trim() === "Episode") {
+			getDataFilms(userSearch, "Episode");
+		}
+	}
+});
