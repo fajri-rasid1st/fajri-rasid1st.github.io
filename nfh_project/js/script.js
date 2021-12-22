272e38cfe592db75b3e39aa3d25d868f7a3f3909
Muhammad Fajri Rasid <fajrimuhammad460@gmail.com>
2021-12-22 23:06:29 +0800

    added button scroll to top

--- nfh_project/js/script.js
+++ nfh_project/js/script.js
@@ -3,3 +3,24 @@ $(".search-icon").on("click", function () {
 	$(".search-field").toggleClass("open-close");
 	$(".search-icon").toggleClass("rotate");
 });
+
+// scroll button to top effect
+$(window).on("scroll", function () {
+	let windowScroll = $(this).scrollTop();
+
+	if (windowScroll > 750) {
+		$(".section-on-top-btn").fadeIn(500);
+	} else {
+		$(".section-on-top-btn").fadeOut(500);
+	}
+});
+
+// scroll button to top animate
+$(".section-on-top-btn").on("click", function () {
+	$("html, body").animate(
+		{
+			scrollTop: 0,
+		},
+		750
+	);
+});
