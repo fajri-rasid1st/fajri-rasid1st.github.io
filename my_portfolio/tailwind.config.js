bcdbd2794181090dfba02d71171007990deec6d2
Muhammad Fajri Rasid <fajrimuhammad460@gmail.com>
2025-02-21 11:48:53 +0800

    update portfolio

--- my_portfolio/tailwind.config.js
+++ my_portfolio/tailwind.config.js
@@ -1,28 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-	content: ["index.html"],
-	darkMode: "class",
-	theme: {
-		container: {
-			center: true,
-			padding: "24px",
-		},
-		extend: {
-			screens: {
-				"2xl": "1320px",
-			},
-			fontFamily: {
-				plusJakartaSans: ["Plus Jakarta Sans", "sans-serif"],
-			},
-			colors: {
-				primary: "#14b8a6",
-				secondary: "#64748b",
-				tertiary: "#94a3b8",
-				dark: "#334155",
-				darkest: "#1e293b",
-			},
-		},
-	},
-	plugins: [],
+  content: ["index.html"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: "24px",
+    },
+    extend: {
+      screens: {
+        "2xl": "1320px",
+      },
+      fontFamily: {
+        plusJakartaSans: ["Plus Jakarta Sans", "sans-serif"],
+      },
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        tertiary: "#9facbf",
+        dark: "#334155",
+        darkest: "#1e293b",
+      },
+    },
+  },
+  plugins: [],
 };
